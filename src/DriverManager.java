4b9d9a81ca2e9c05942f9d1afe04f952f3975746
Merey4547 <230936@astanait.edu.kz>
2024-10-02 20:42:10 +0500

    create a private static instance of the class

---
+++ src/DriverManager.java
@@ -0,0 +1,9 @@
+public class DriverManager {
+    private static DriverManager instance;
+
+
+    public String findNearestDriver(String location) {
+        System.out.println("Nearest driver found: " + "Driver1");
+        return "Driver1";
+    }
+}
