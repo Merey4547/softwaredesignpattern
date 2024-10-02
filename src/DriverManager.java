1ee5fc9100df2521df0bb8c018930769b089fd4b
Merey4547 <230936@astanait.edu.kz>
2024-10-02 20:45:54 +0500

    Provide a public static method to get the instance of the class

--- src/DriverManager.java
+++ src/DriverManager.java
@@ -4,6 +4,13 @@ public class DriverManager {
     private DriverManager() {
     }
 
+    public static DriverManager getInstance() {
+        if (instance == null) {
+            instance = new DriverManager();
+        }
+        return instance;
+    }
+
     public String findNearestDriver(String location) {
         System.out.println("Nearest driver found: " + "Driver1");
         return "Driver1";
