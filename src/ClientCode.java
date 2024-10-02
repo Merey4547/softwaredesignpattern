31144f5ed204cd9da03ec937d162d673e6df177d
Merey4547 <230936@astanait.edu.kz>
2024-10-02 22:19:34 +0500

    таск4

--- src/ClientCode.java
+++ src/ClientCode.java
@@ -11,4 +11,12 @@ public static void main(String[] args) {
     clonedDriver.setName("Jane Doe");
     System.out.println("Cloned Driver: " + clonedDriver.getName());
     System.out.println("Cloned Driver’s car: " + clonedDriver.getCarModel());
-}
+
+        Factory factory = new StandardTaxiFactory();
+        Taxi taxi = factory.createTaxi("Daniele", "XYZ12345");
+        taxi.takePassenger("John");
+
+        factory = new PremiumTaxiFactory();
+        taxi = factory.createTaxi("Daniele", "XYZ12345");
+        taxi.takePassenger("Doe");
+    }
