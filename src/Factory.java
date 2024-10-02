e5ba5db2c2a9740865a02c96cbf532c7af23f73a
Merey4547 <230936@astanait.edu.kz>
2024-10-02 22:06:35 +0500

    заново

--- src/Factory.java
+++
@@ -1,3 +0,0 @@
-public abstract class Factory {
-    public abstract Taxi createTaxi(String driverName, String license);
-}
--- src/PremiumTaxiFactory.java
+++
@@ -1,6 +0,0 @@
-public class PremiumTaxiFactory extends Factory {
-    @Override
-    public Taxi createTaxi(String driverName, String license) {
-        return new PremiumTaxi(driverName, license);
-    }
-}
