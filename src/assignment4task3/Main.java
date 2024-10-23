13939d79882ff8484cec01d3dad8a30613ab1e7b
Merey4547 <230936@astanait.edu.kz>
2024-10-24 02:24:11 +0800

    assign4task3

---
+++ src/assignment4task3/Main.java
@@ -0,0 +1,50 @@
+package assignment4task3;
+
+interface PricingStrategy {
+    double calculatePrice(double value);
+}
+
+class DistanceStrategy implements PricingStrategy {
+    public double calculatePrice(double distance) {
+        return distance * 10; // Price per km
+    }
+}
+
+class TimeStrategy implements PricingStrategy {
+    public double calculatePrice(double time) {
+        return time * 2; // Price per minute
+    }
+}
+
+class FixedPriceStrategy implements PricingStrategy {
+    public double calculatePrice(double value) {
+        return 50; // Fixed price
+    }
+}
+
+class TaxiOrder {
+    private PricingStrategy strategy;
+
+    public void setStrategy(PricingStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public double calculateCost(double value) {
+        return strategy.calculatePrice(value);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        TaxiOrder order = new TaxiOrder();
+
+        order.setStrategy(new DistanceStrategy());
+        System.out.println("Cost by distance: " + order.calculateCost(15));
+
+        order.setStrategy(new TimeStrategy());
+        System.out.println("Cost by time: " + order.calculateCost(30));
+
+        order.setStrategy(new FixedPriceStrategy());
+        System.out.println("Fixed cost: " + order.calculateCost(0));
+    }
+}
